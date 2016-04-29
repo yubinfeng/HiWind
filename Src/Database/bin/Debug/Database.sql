306f6cb6af8fa222d30905d1a70d8672e1a43a82
yubinfeng <bifke@foxmail.com>
2016-04-29 21:59:29 +0800

    更新1.1.0429

---
+++ Src/Database/bin/Debug/Database.sql
@@ -0,0 +1,46 @@
+﻿/*
+Database 的部署脚本
+
+此代码由工具生成。
+如果重新生成此代码，则对此文件的更改可能导致
+不正确的行为并将丢失。
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Database"
+:setvar DefaultFilePrefix "Database"
+:setvar DefaultDataPath "C:\Users\Administrator\AppData\Local\Microsoft\VisualStudio\SSDT\HiWind"
+:setvar DefaultLogPath "C:\Users\Administrator\AppData\Local\Microsoft\VisualStudio\SSDT\HiWind"
+
+GO
+:on error exit
+GO
+/*
+请检测 SQLCMD 模式，如果不支持 SQLCMD 模式，请禁用脚本执行。
+要在启用 SQLCMD 模式后重新启用脚本，请执行:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'要成功执行此脚本，必须启用 SQLCMD 模式。';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+PRINT N'更新完成。';
+
+
+GO
